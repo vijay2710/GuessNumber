2f2e4628ce06812b09d2f67784eb3ee21fa26f16
vijay muralidharan <vijay muralidharan>
2016-03-09 14:57:52 +0000

    Implement algorithm

--- GuessNumber.java
+++ GuessNumber.java
@@ -1,12 +1,47 @@
-public class GuessNumber{
+import java.util.Scanner;
 
+
+
+
+public class GuessNumber{
+    private static class Guesser{       
+    private int lower;
+    private int upper;
+    //define constructor
+    public Guesser(int l,int u){
+    	lower = l;
+    	upper = u;
+      
+
+    }
+    public void quiz(){
+    	System.out.println("Think of a number between " + lower + " and "  + upper );
+    	int temp;
+    	Scanner sc = new Scanner(System.in);
+    		int l = lower;
+    		int u = upper;
+    		while(l!=u){
+    		temp = l + (u-l)/2;
+    		System.out.println("is it greater than " + temp +" ? ");
+    		if(sc.next().equals("y")){
+            l = temp+1;
+    		}
+    		else{
+            u = temp;
+    		}
+
+    	}
+    System.out.println("Your number is " + u);
+    }
+   
+    }
 	public static void main(String[] args){
-		Guesser g=new Guesser();
+	//	Guesser g=new Guesser();
 		int lower= 0;
 		int upper = 16;
-		g.newGame(lower,upper);
+		Guesser g = new Guesser(lower,upper);
 		g.quiz();
-		
+
 
 	}
 }
